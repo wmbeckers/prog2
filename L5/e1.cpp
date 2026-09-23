a0cd807bf3515669ae0a4a4611ab1363ab3f0bbf
Murilo <64659295+wmbeckers@users.noreply.github.com>
2026-09-23 16:22:40 -0300

    Add basic C++ program to display variable addresses

---
+++ L5/e1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 20;
+
+	cout << "x = " << x << ", endereco de x = " << &x << endl;
+	cout << "y = " << y << ", endereco de y = " << &y << endl;
+
+	return 0;
+}
